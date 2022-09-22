a7ceaf90d3568928bc75ce7cb0013f2fba0b73b4
RamunasKybartas <kybartas.ramunas@gmail.com>
2022-09-22 21:34:29 +0300

    added new task

--- silta_salta.py
+++ silta_salta.py
@@ -10,6 +10,46 @@
 # * Programą kartoti, kol būna atspėtas skaičius. Atspėjus, išpsausdinti sveikinimus ir teisingą atsakymą.
 # * BONUS 1: teisingas skaičius parenkamas atsitiktiniu būdu, įskaitant neigiamus skaičius. Pradžiai vistiek siūlyčiau gan mažą imtį.
 # * BONUS 2: skaičiuokite kiek kartų buvo spėta
+from random import randint
 
-correct_number = 19
-guess_number = int(input("Iveskite skaiciu: ")) 
+old_difference = 0
+correct_number = randint(-20, 50)
+guess_count = 0
+while True:
+    try:
+        guess_number = int(input("Iveskite skaiciu: "))
+    except ValueError:
+        print("Ivestis turi buti sveikasis skaicius")
+    guess_count += 1
+    if guess_number == correct_number:
+        print(f"Sveikiname!, laimingas skaicius {correct_number} atspeta is {guess_count} kartu")
+        break
+    difference = abs(correct_number - guess_number)
+    if difference < old_difference:
+        old_difference = difference
+        print("silta")
+    else:
+        print("salta")
+        old_difference = difference
+
+
+# from random import randint
+# teisingas = randint(-2**15, 2**15) # 777
+# spejimas = None
+# skirtumas = 0
+# kartai = 0
+# while spejimas is None or spejimas != teisingas:
+#     try:
+#         spejimas = int(input("Spėkite skaičių: "))
+#     except ValueError:
+#         print("įvestas ne skaičius")
+#     else:
+#         if teisingas != spejimas:
+#             kartai += 1
+#             if abs(teisingas - spejimas) < skirtumas:
+#                 print(spejimas, "šilta...")
+#             else:
+#                 print(spejimas, "šalta...")
+#             skirtumas = abs(teisingas - spejimas)
+# else:
+#     print(f"!!! Atspėjai iš {kartai} karto. Teisingas atsakymas: {teisingas}")
